0463190ff3ca2f8c3d65bfe8b2cc4012950ddc9c
Dima Muzychenko <robin700521@gmail.com>
2024-03-05 22:28:18 +0200

    ready

--- src/FileChoose.java
+++ src/FileChoose.java
@@ -8,11 +8,8 @@ public class FileChoose {
 
         // Set the file dialog to open only .txt files (optional)
         fileDialog.setFile("*.txt");
-
-        // Display the file dialog
         fileDialog.setVisible(true);
 
-        // Get the chosen file
         String directory = fileDialog.getDirectory();
         String file = fileDialog.getFile();
         String filePath = null;
@@ -20,12 +17,12 @@ public class FileChoose {
         if (directory != null && file != null) {
             // Combine directory and file to get the full path
             filePath = directory + file;
-
-            // Now you can use the filePath variable to access the chosen file
             System.out.println("Selected file: " + filePath);
         } else {
             throw new FileNotFoundException();
         }
+
+        fileDialog.dispose();
         return filePath;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,8 @@ public class Main {
             //choose necessary file
             FileChoose fc = new FileChoose();
             String fileStr = fc.chooseFile();
+            //start time measuring
+            long startTime = System.currentTimeMillis();
 
             File file = new File(fileStr);
             Scanner scanner = new Scanner(file).useDelimiter("[\\s,.]+"); //s-пробіл
@@ -22,24 +24,31 @@ public class Main {
             scanner.close();
 
             FindInFile findInFile = new FindInFile();
-            int max = findInFile.getMax(numbers);
-            int min = findInFile.getMin(numbers);
-            double median = findInFile.findMedian(new ArrayList<>(numbers));//копія
-            double average = findInFile.findAverage(numbers);
-            ArrayList<Integer> increasingSequence = findInFile.findIncreasingSequence(numbers);
-            ArrayList<Integer> decreasingSequence = findInFile.findDecreasingSequence(numbers);
+            if (!numbers.isEmpty()) {
+                int max = findInFile.getMax(numbers);
+                int min = findInFile.getMin(numbers);
+                double median = findInFile.findMedian(new ArrayList<>(numbers));//копія
+                double average = findInFile.findAverage(numbers);
+                ArrayList<Integer> increasingSequence = findInFile.findIncreasingSequence(numbers);
+                ArrayList<Integer> decreasingSequence = findInFile.findDecreasingSequence(numbers);
 
+                long endTime = System.currentTimeMillis();
+                long timePassed = endTime - startTime;
+                double seconds = (double) timePassed / 1000.0;
+                System.out.println("Time passed: " + seconds + " seconds");
 
-            System.out.println("Максимальне число: " + max);
-            System.out.println("Мінімальне число: " + min);
-            System.out.println("Медіана: " + median);
-            System.out.println("Середнє арифметичне: " + String.format("%.3f", average));
-            System.out.println("Найбільша послідовність чисел, яка збільшується: " + increasingSequence);
-            System.out.println("Найбільша послідовність чисел, яка зменшується: " + decreasingSequence);
-
+                System.out.println("Максимальне число: " + max);
+                System.out.println("Мінімальне число: " + min);
+                System.out.println("Медіана: " + median);
+                System.out.println("Середнє арифметичне: " + String.format("%.3f", average));
+                System.out.println("Найбільша послідовність чисел, яка збільшується: " + increasingSequence);
+                System.out.println("Найбільша послідовність чисел, яка зменшується: " + decreasingSequence);
+            }else {
+                System.out.println("Файл містить не підходящі дані");
+            }
         } catch (FileNotFoundException e) {
             System.out.println("Файл не знайдено.");
-            e.printStackTrace();
         }
+        System.exit(0);
     }
 }
